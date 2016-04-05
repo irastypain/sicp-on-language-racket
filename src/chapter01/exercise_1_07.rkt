20966dc009d1de5d6083b8a57cb644bc3e81c6bb
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-05 11:06:36 +0300

    Added exercise_1_07 and update doc/chapter01/index.md

---
+++ src/chapter01/exercise_1_07.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+(require "../lib/base.rkt")
+
+; Набор функций для вычисления квадратного корня методом Ньютона
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; Экспорт функции
+(provide sqrt)
+
+; Набор функций для улучшенного вычисления квадратного корня методом Ньютона
+
+(define (better-good-enough? guess prev-guess)
+  (< (abs (/ (- guess prev-guess)
+             prev-guess))
+     0.0000001))
+
+(define (better-sqrt-iter guess prev-guess x)
+  (if (better-good-enough? guess prev-guess)
+      guess
+      (better-sqrt-iter (improve guess x)
+                        guess
+                        x)))
+
+(define (better-sqrt x)
+  (better-sqrt-iter 1.0 0.5 x))
+
+; Экспорт функции
+(provide better-sqrt)
