7d67bfe781e98dbc6b9f122cf2f824fad28500e9
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-05 22:19:43 +0300

    Fixed text in document

--- src/chapter01/exercise_1_08.rkt
+++ src/chapter01/exercise_1_08.rkt
@@ -2,24 +2,23 @@
 
 (require "../lib/base.rkt")
 
-; Набор функций для вычисления кубического корня
-
+; Набор процедур для вычисления кубического корня
 (define (cbrt x)
   (define (cbrt-iter guess)
     (if (good-enough? guess)
         guess
         (cbrt-iter (improve guess))))
-  
+
   (define (improve guess)
     (/ (+ (/ x (square guess))
           (* 2 guess))
        3))
-  
+
   (define (good-enough? guess)
     (< (abs (- (cube guess) x))
        0.0000001))
-  
+
   (cbrt-iter 1.0))
 
-; Экспорт функции
+; Экспорт процедуры
 (provide cbrt)
--- test/chapter01/test_exercise_1_07.rkt
+++ test/chapter01/test_exercise_1_07.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(require rackunit "../../src/lib/base.rkt" "../../src/chapter01/exercise_1_07.rkt")
+(require rackunit
+         "../../src/lib/base.rkt"
+         "../../src/chapter01/exercise_1_07.rkt")
 
 ; Тесты для примеров из книги
 (check-equal? (sqrt 9) 3.00009155413138 (printf "test-from-book#1 1.7 passed\n"))
@@ -8,7 +10,7 @@
 (check-equal? (sqrt (+ (sqrt 2) (sqrt 3))) 1.7739279023207892 (printf "test-from-book#3 1.7 passed\n"))
 (check-equal? (square (sqrt 1000)) 1000.000369924366 (printf "test-from-book#4 1.7 passed\n"))
 
-; Тесты для улучшенной функции
+; Тесты для улучшенной процедуры
 (check-equal? (better-sqrt 9) 3.0 (printf "test#1 1.7 passed\n"))
 (check-equal? (better-sqrt 0.09) 0.3 (printf "test#2 1.7 passed\n"))
 (check-equal? (square (better-sqrt 1000)) 1000.0 (printf "test#3 1.7 passed\n"))
