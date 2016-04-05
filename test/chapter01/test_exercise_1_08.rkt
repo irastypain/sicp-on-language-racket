1bd565e508e443d8f065920b8e12301ec861b628
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-05 12:26:24 +0300

    Fix test/chapter01/test_exercise_1_08.rkt

--- test/chapter01/test_exercise_1_08.rkt
+++ test/chapter01/test_exercise_1_08.rkt
@@ -8,4 +8,4 @@
 (check-= (cbrt 8) 2.0 0.0000001 (printf "test#1 1.8 passed\n"))
 (check-= (cbrt 27) 3.0 0.0000001 (printf "test#2 1.8 passed\n"))
 (check-= (cbrt 64) 4.0 0.0000001 (printf "test#3 1.8 passed\n"))
-(check-= (cbrt 125) 5.0 0.0000001 (printf "test#3 1.8 passed\n"))
+(check-= (cbrt 125) 5.0 0.0000001 (printf "test#4 1.8 passed\n"))
