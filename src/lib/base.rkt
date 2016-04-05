da05dbbce340b8aee6346b8827e234ccab80fc21
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-05 12:19:27 +0300

    Update doc/chapter01/index.md, images/chapter01/exercise_1_02.gif, src/lib/base.rkt

--- src/lib/base.rkt
+++ src/lib/base.rkt
@@ -5,6 +5,11 @@
 ; Экспорт функции
 (provide square)
 
+; Функция возводит число 'x' в куб
+(define (cube x) (* x x x))
+; Экспорт функции
+(provide cube)
+
 ; Функция вычисляет сумму квадратов чисел 'x' и 'y'
 (define (sum-of-squares x y)
   (+ (square x)
