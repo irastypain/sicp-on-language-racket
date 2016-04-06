d18e94ca4fbf750bb1b9636a8cf5f2a5c50e95c0
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-06 17:46:04 +0300

    Added exercise_1_14

---
+++ src/chapter01/exercise_1_14.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+; Набор процедур для вычисления количества вариантов размена
+; суммы монетами достоинством в 1, 5, 10, 25 и 50 единиц.
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                    (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+; Экспорт процедуры
+(provide count-change)
