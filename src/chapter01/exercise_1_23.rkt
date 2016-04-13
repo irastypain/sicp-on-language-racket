5ee5749fb5412287042ecacca45e95386958e97e
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-13 13:04:07 +0300

    Added exercise_1_23

---
+++ src/chapter01/exercise_1_23.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require rackunit "../lib/better-prime.rkt")
+
+; Набор процедур для проверки числа на простоту.
+; В случае, если число простое, выводится время работы процедуры
+; Импортируем улучшенную версию проверки числа на простоту `prime?`
+(define (timed-prime-test number)
+  (start-prime-test number (current-inexact-milliseconds)))
+
+(define (start-prime-test number start-time)
+  (if (prime? number)
+      (report-prime (- (current-inexact-milliseconds) start-time) number)
+      false))
+
+(define (report-prime elapsed-time number)
+  (display "Number ")
+  (display number)
+  (display " is simple. Time compute: ")
+  (display elapsed-time)
+  (display "ms.")
+  (newline))
+
+; Экспорт процедуры
+(provide timed-prime-test)
