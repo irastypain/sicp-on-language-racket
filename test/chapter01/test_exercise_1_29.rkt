0057c77dfc06cc1c36769510e6d2d8f8d3bf65ea
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-27 22:11:26 +0300

    Fix exercise_1_29.md and test_exercise_1_29.rkt

--- test/chapter01/test_exercise_1_29.rkt
+++ test/chapter01/test_exercise_1_29.rkt
@@ -4,6 +4,5 @@
          "../../src/lib/base.rkt"
          "../../src/chapter01/exercise_1_29.rkt")
 
-; Проверяем с точностью до 0.0000001
 (check-equal? (simpson cube 0 1 100.0) 0.24999999999999992 (printf "test#1 1.29 passed\n"))
 (check-equal? (simpson cube 0 1 1000.0) 0.2500000000000003 (printf "test#2 1.29 passed\n"))
