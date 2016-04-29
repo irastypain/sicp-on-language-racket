73158d70e2fbe9de052c7b6fdb2798b63374b636
Ivan Rastyapin <i.rastypain@gmail.com>
2016-04-29 14:53:46 +0300

    Added exercise_1_37

---
+++ test/chapter01/test_exercise_1_37i.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require rackunit "../../src/chapter01/exercise_1_37i.rkt")
+
+(check-= (count-iterations (lambda (k) (/ 1
+                                 (cont-frac (lambda (i) 1.0)
+                                            (lambda (i) 1.0)
+                                            k)))
+                           1.618033988749895
+                           0.00001)
+         1.618033988749895
+         0.00001
+         (printf "test#1 1.37 passed\n"))
