e4ec674903ad816f6733b306ece660feb6fc4905
Ivan Rastyapin <i.rastypain@gmail.com>
2016-05-02 21:40:07 +0300

    Added exercise_1_42

---
+++ test/chapter01/test_exercise_1_42.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+(require rackunit
+         "../../src/lib/base.rkt"
+         "../../src/chapter01/exercise_1_42.rkt")
+
+(check-equal? ((compose square inc) 6) 49 (printf "test#1 1.42 passed\n"))
+(check-equal? ((compose (compose inc square) inc) 6) 50 (printf "test#2 1.41 passed\n"))
+(check-equal? ((compose (compose square inc) square) 5) 676 (printf "test#3 1.41 passed\n"))
