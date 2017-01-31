a42edf268cd307c76169ab7a2906c3bccf447b79
Ivan Rastyapin <i.rastypain@gmail.com>
2017-01-31 21:35:16 +0300

    Fix function in exercise_1_11i.rkt

--- src/chapter01/exercise_1_11i.rkt
+++ src/chapter01/exercise_1_11i.rkt
@@ -4,7 +4,7 @@
 (define (iter-fn a b c count)
   (if (= count 0)
       c
-      (+ (iter-fn (+ a b c) a b (- count 1)))))
+      (iter-fn (+ a b c) a b (- count 1))))
 
 (define (fn n)
   (iter-fn 2 1 0 n))
