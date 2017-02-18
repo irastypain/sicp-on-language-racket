0ab4a950435119a2cb2c3d5b3f67e706bd367316
Ivan Rastyapin <i.rastypain@gmail.com>
2017-02-18 22:26:51 +0300

    Added test-case in test/chapter01/test_exercise_1_39.rkt

--- test/chapter01/test_exercise_1_39.rkt
+++ test/chapter01/test_exercise_1_39.rkt
@@ -1,6 +1,15 @@
 #lang racket
 
-(require rackunit "../../src/chapter01/exercise_1_39.rkt")
+(require rackunit
+         "../../src/chapter01/exercise_1_39.rkt"
+         "../../src/lib/count-iterations.rkt")
 
-(check-= (tan-cf 1.0 10) 1.557407724 0.000001 (printf "test#1 1.39 passed\n"))
-(check-= (tan-cf 1.0 1000) 1.557407724 0.0000001 (printf "test#2 1.39 passed\n"))
+(define tan1.0
+  (lambda (k) (tan-cf 1.0 k)))
+
+(define minIterations
+  (count-iterations tan1.0 1.557407724 0.000001))
+
+(check-= (tan1.0 minIterations) 1.557407724 0.000001 (printf "test#1 1.39 passed\n"))
+(check-= (tan-cf 1.0 10) 1.557407724 0.000001 (printf "test#2 1.39 passed\n"))
+(check-= (tan-cf 1.0 1000) 1.557407724 0.000000001 (printf "test#3 1.39 passed\n"))
