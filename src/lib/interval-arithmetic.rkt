cdcb15762cb03ac24ab53a82bab71f9efd533d3f
Ivan Rastyapin <i.rastypain@gmail.com>
2017-03-13 22:26:16 +0300

    Fix lib: interval-arithmetic

--- src/lib/interval-arithmetic.rkt
+++ src/lib/interval-arithmetic.rkt
@@ -84,13 +84,18 @@
 ; Конструктор интервала по серединному
 ; значению интервала и погрешностью в процентах
 (define (make-center-percent c p)
-  (let ((additive-error (* c p)))
-    (make-center-width c additive-error)))
+  (let ((percent (/ p 100)))
+    (make-center-width c
+                       (if (= c 0) percent
+                           (* (abs c) percent)))))
 
 ; Получение погрешности в процентах
 (define (percent i)
-  (let ((c (center i)))
-    (/ c (- (upper-bound i) c))))
+  (let ((c (center i))
+        (w (* (width i) 100)))
+    (cond ((= c 0) w)
+          ((= w 0) 0)
+          (else (abs (/ w c))))))
 
 ; Экспорт процедур
 (provide make-interval
