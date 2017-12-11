3ebc09ba261569099048d3710b7d6a81ec25e95e
Ivan Rastyapin <i.rastypain@gmail.com>
2017-12-11 20:00:37 +0300

    Add exercise_2_46

---
+++ src/chapter02/exercise_2_46.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+; Конструктор вектора
+(define make-vect cons)
+
+; Селектор координаты X
+(define xcor-vect car)
+
+; Селектор координаты Y
+(define ycor-vect cdr)
+
+; Сумма векторов
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
+; Разность векторов
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+
+; Умножение скаляра на вектор
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+; Экспорт процедур
+(provide make-vect
+         xcor-vect
+         ycor-vect
+         add-vect
+         sub-vect
+         scale-vect)
