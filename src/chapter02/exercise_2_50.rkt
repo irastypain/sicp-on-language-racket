584d71809276f131d1171c9400b72028ea29b19b
Ivan Rastyapin <i.rastypain@gmail.com>
2017-12-12 23:52:51 +0300

    Add exercise_2_50

---
+++ src/chapter02/exercise_2_50.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require (only-in "../lib/graphics.rkt"
+                  transform-painter))
+
+; Процедура отображает painter зеркально горизонтально
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+; Процедура поворачивает painter на 180 градусов
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+; Процедура поворачивает painter на 270 градусов
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
