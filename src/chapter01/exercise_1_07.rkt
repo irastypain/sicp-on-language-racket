d17db89b1dc949f3f0e240183ec7dbd53dde8685
Ivan Rastyapin <i.rastypain@gmail.com>
2019-10-26 00:29:57 +0300

    Improve tests for exercise_1_07

--- src/chapter01/exercise_1_07.rkt
+++ src/chapter01/exercise_1_07.rkt
@@ -1,41 +1,26 @@
 #lang racket
 
-(require "../lib/base.rkt")
+(require (only-in "../../lib/base.rkt"
+                  abs
+                  average
+                  square))
 
-; Набор процедур для вычисления квадратного корня методом Ньютона
+(define (improved-sqrt x)
+  (improved-sqrt-iter 1.0 0.5 x))
+
+(define (improved-sqrt-iter guess prev-guess x)
+  (if (improved-good-enough? guess prev-guess x)
+    guess
+    (improved-sqrt-iter (improve guess x)
+               guess
+               x)))
+
+(define (improved-good-enough? guess prev-guess x)
+  (< (abs (/ (- guess prev-guess)
+                prev-guess))
+          0.00001))
+
 (define (improve guess x)
   (average guess (/ x guess)))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x)
-                 x)))
-
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
-
-; Экспорт процедуры
-(provide sqrt)
-
-; Набор процедур для улучшенного вычисления квадратного корня методом Ньютона
-(define (better-good-enough? guess prev-guess)
-  (< (abs (/ (- guess prev-guess)
-             prev-guess))
-     0.0000001))
-
-(define (better-sqrt-iter guess prev-guess x)
-  (if (better-good-enough? guess prev-guess)
-      guess
-      (better-sqrt-iter (improve guess x)
-                        guess
-                        x)))
-
-(define (better-sqrt x)
-  (better-sqrt-iter 1.0 0.5 x))
-
-; Экспорт процедуры
-(provide better-sqrt)
+(provide improved-sqrt)
--- test/chapter01/test_exercise_1_07.rkt
+++ test/chapter01/test_exercise_1_07.rkt
@@ -1,18 +1,19 @@
 #lang racket
 
 (require rackunit
-         "../../src/lib/const.rkt"
-         "../../src/lib/base.rkt"
+         rackunit/text-ui
          "../../src/chapter01/exercise_1_07.rkt")
 
-; Тесты для примеров из книги
-(check-equal? (sqrt 9) 3.00009155413138 (printf "test-from-book#1 1.7 passed\n"))
-(check-equal? (sqrt (+ 100 37)) 11.704699917758145 (printf "test-from-book#2 1.7 passed\n"))
-(check-equal? (sqrt (+ (sqrt 2) (sqrt 3))) 1.7739279023207892 (printf "test-from-book#3 1.7 passed\n"))
-(check-equal? (square (sqrt 1000)) 1000.000369924366 (printf "test-from-book#4 1.7 passed\n"))
+(define tests
+  (test-suite
+    "Improved `sqrt` procedure for biggest and smallest numbers"
 
-; Тесты для улучшенной процедуры
-(check-= (better-sqrt 9) 3.0 tolerance (printf "test#1 1.7 passed\n"))
-(check-= (better-sqrt 0.09) 0.3 tolerance (printf "test#2 1.7 passed\n"))
-(check-= (square (better-sqrt 1000)) 1000.0 tolerance (printf "test#3 1.7 passed\n"))
-(check-= (better-sqrt (+ (better-sqrt 9) (better-sqrt 4))) 2.23606797749979 tolerance (printf "test#4 1.7 passed\n"))
+    (test-case
+      "When smallest number"
+      (check-= 0.3 (improved-sqrt 0.09) 0.00001))
+
+    (test-case
+      "When biggest number"
+      (check-= 1000.0 (improved-sqrt 1000000) 0.00001))))
+
+(run-tests tests 'verbose)
