14f497a4d8e3e86b893441a5fcc03ad160456985
Ivan Rastyapin <i.rastypain@gmail.com>
2019-10-26 01:05:05 +0300

    Use new `base` library in exercise_1_03

--- src/chapter01/exercise_1_03.rkt
+++ src/chapter01/exercise_1_03.rkt
@@ -1,10 +1,14 @@
 #lang racket
 
-(require "../lib/base.rkt")
+(require (only-in "../../lib/base.rkt"
+                  square))
 
 (define (sum-of-squares-of-greatest-two-numbers a b c)
   (if (= a (max a b))
       (sum-of-squares a (max b c))
       (sum-of-squares b (max a c))))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
 
 (provide sum-of-squares-of-greatest-two-numbers)
