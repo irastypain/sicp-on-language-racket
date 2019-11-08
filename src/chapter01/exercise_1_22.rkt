9a90959c159123c6568165d96b536399eb8a4959
Ivan Rastyapin <i.rastypain@gmail.com>
2019-11-08 22:05:55 +0300

    Improve exercise_1_22

--- src/chapter01/exercise_1_22.rkt
+++ src/chapter01/exercise_1_22.rkt
@@ -1,16 +1,32 @@
 #lang racket
 
-(require "../lib/prime.rkt")
+(require (only-in "../../lib/base.rkt"
+                  divides?
+                  square))
 
-; Набор процедур для проверки числа на простоту.
-; В случае, если число простое, выводится время работы процедуры
+(define (search-for-primes number prime-count)
+  (cond ((> prime-count 0)
+         (cond ((timed-prime-test number)
+                (search-for-primes (+ number 1) (- prime-count 1)))
+               (else (search-for-primes (+ number 1) prime-count))))))
+
 (define (timed-prime-test number)
   (start-prime-test number (current-inexact-milliseconds)))
 
 (define (start-prime-test number start-time)
   (if (prime? number)
-      (report-prime (- (current-inexact-milliseconds) start-time) number)
-      false))
+    (report-prime (- (current-inexact-milliseconds) start-time) number)
+    false))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (smallest-divisor n)
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (find-divisor n 2))
 
 (define (report-prime elapsed-time number)
   (display "Number ")
@@ -20,15 +36,4 @@
   (display "ms.")
   (newline))
 
-; Экспорт процедуры
-(provide timed-prime-test)
-
-; Процедура поиска `n` простых чисел после заданного числа
-(define (search-for-primes number prime-count)
-  (cond ((> prime-count 0)
-         (cond ((timed-prime-test number)
-                (search-for-primes (+ number 1) (- prime-count 1)))
-               (else (search-for-primes (+ number 1) prime-count))))))
-
-; Экспорт процедуры
 (provide search-for-primes)
